b833e41e73fd2dddb34c810b3db9a585588cacc1
uintdev <core@uint.dev>
2017-05-04 23:17:11 +0100

    Initial post

---
+++ Debug/src/asf/avr32/drivers/spi/spi.d
@@ -0,0 +1,156 @@
+src/asf/avr32/drivers/spi/spi.d src/asf/avr32/drivers/spi/spi.o \
+ src/asf/avr32/drivers/spi/spi.o: ../src/asf/avr32/drivers/spi/spi.c \
+ ../src/asf/avr32/drivers/spi/spi.h ../src/asf/avr32/utils/compiler.h \
+ ../src/asf/avr32/utils/preprocessor/preprocessor.h \
+ ../src/asf/avr32/utils/preprocessor/tpaste.h \
+ ../src/asf/avr32/utils/preprocessor/stringz.h \
+ ../src/asf/avr32/utils/preprocessor/mrepeat.h \
+ ../src/asf/avr32/utils/preprocessor/preprocessor.h \
+ ../src/asf/avr32/utils/parts.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/io.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/uc3b0256.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/core_sc0_130.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/abi.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/ocd_s0_130.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/intc_101.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/adc_200.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/eic_230.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/flashc_200.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/freqm_230.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/gpio_110.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/hmatrix_230.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pdca_102.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pm_230.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pwm_130.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/rtc_230.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/spi_199.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/ssc_310.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/tc_222.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/twi_211.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/usart_400.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/usbb_310.h \
+ C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/wdt_230.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stddef.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/newlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/config.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/ieeefp.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/reent.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/_types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_default_types.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/lock.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/stdlib.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/alloca.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stdbool.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdint.h \
+ ../src/asf/common/utils/interrupt.h ../src/asf/avr32/utils/parts.h \
+ ../src/asf/common/utils/interrupt/interrupt_avr32.h \
+ ../src/asf/avr32/utils/preprocessor/tpaste.h \
+ ../src/asf/avr32/drivers/intc/intc.h
+
+../src/asf/avr32/drivers/spi/spi.h:
+
+../src/asf/avr32/utils/compiler.h:
+
+../src/asf/avr32/utils/preprocessor/preprocessor.h:
+
+../src/asf/avr32/utils/preprocessor/tpaste.h:
+
+../src/asf/avr32/utils/preprocessor/stringz.h:
+
+../src/asf/avr32/utils/preprocessor/mrepeat.h:
+
+../src/asf/avr32/utils/preprocessor/preprocessor.h:
+
+../src/asf/avr32/utils/parts.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/io.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/uc3b0256.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/core_sc0_130.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/abi.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/ocd_s0_130.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/intc_101.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/adc_200.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/eic_230.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/flashc_200.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/freqm_230.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/gpio_110.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/hmatrix_230.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pdca_102.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pm_230.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/pwm_130.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/rtc_230.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/spi_199.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/ssc_310.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/tc_222.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/twi_211.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/usart_400.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/usbb_310.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\UC3B_DFP\1.0.29\include\AT32UC3B0256/avr32/wdt_230.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stddef.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/newlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/config.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/ieeefp.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/reent.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/_ansi.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/_types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/_default_types.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/sys/lock.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/machine/stdlib.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/alloca.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/include/stdbool.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr32\avr32-gnu-toolchain\bin\../lib/gcc/avr32/4.4.7/../../../../avr32/include/stdint.h:
+
+../src/asf/common/utils/interrupt.h:
+
+../src/asf/avr32/utils/parts.h:
+
+../src/asf/common/utils/interrupt/interrupt_avr32.h:
+
+../src/asf/avr32/utils/preprocessor/tpaste.h:
+
+../src/asf/avr32/drivers/intc/intc.h:
